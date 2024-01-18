442827317f30e164cd4faf95027a07d50666e098
KinshasaPolee <kpolee1@gmail.com>
2024-01-18 15:44:41 -0500

    updated js

--- index.js
+++ index.js
@@ -2,45 +2,63 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ extended: true }));
 // Importing the data from our fake database files.
 const users = require("./data/users");
 const posts = require("./data/posts");
 
-// index route for Users
+// // index route for Users
+// app.get("/api/users", (req, res) => {
+//     res.json(users);
+// });
 
-app.get("/api/users", (req, res) => {
-    res.json(users);
-});
+// // show route for users
+// app.get("/api/users/:id", (req, res) => {
+//     const user = users.find((u) => u.id == req.params.id);
+//     if (user) {
+//         res.json(user);
+//     } else {
+//         res.send("User Not Found")
+//     }
+// });
 
-// show route for users
-app.get("/api/users/:id", (req, res) => {
-    const user = users.find((u) => u.id == req.params.id);
-    if (user) {
-        res.json(user);
-    } else {
-        res.send("User Not Found")
-    }
-});
+// // index route for Posts
+// app.get("/api/posts", (req, res) => {
+//     res.json(posts);
+// });
 
-// index route for Posts
-app.get("/api/posts", (req, res) => {
-    res.json(posts);
-});
+// // show route for posts
+// app.get("/api/posts/:id", (req, res) => {
+//     const post = posts.find((p) => p.id == req.params.id);
+//     if (post) {
+//         res.json(post);
+//     } else {
+//         res.send("Post not found")
+//     }
+// });
 
-// show route for posts
-app.get("/api/posts/:id", (req, res) => {
-    const post = posts.find((p) => p.id == req.params.id);
-    if (post) {
-        res.json(post);
-    } else {
-        res.send("Post not found")
-    }
-});
+// app.get("/", (req, res) => {
+//     res.send("All useable routes start with slash api");
+// });
 
-app.get("/", (req, res) => {
-    res.send("All useable routes start with slash api");
-});
 
-app.listen(port, () => {
-    console.log(`Server listening on port: ${port}.`);
-});
+// all routes
+app
+    .route("/api/users")
+    .get((req, res) => {
+        res.json(users);
+    })
+    .post((req, res) => {
+        if (req.body.name && req.body.username && req.body.email) {
+            if (users.find((u) => u.username == req.body.username)) {
+                res.json({ error: "Username Already Taken" });
+                return;
+            }
+
+            // port listening info
+            app.listen(port, () => {
+                console.log(`Server listening on port: ${port}.`);
+            });
