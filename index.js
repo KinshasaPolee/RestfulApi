b2b0fc634f439f11895b9015353fbc9683d3f424
KinshasaPolee <kpolee1@gmail.com>
2024-01-19 13:38:05 -0500

    updated

--- index.js
+++ index.js
@@ -1,64 +1,118 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
+const users = require("./routes/users");
+const posts = require("./routes/posts");
+
+const error = require("./utilities/error");
+
 const app = express();
 const port = 3000;
 
-const bodyParser = require("body-parser");
-
+// Parsing Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ extended: true }));
-// Importing the data from our fake database files.
-const users = require("./data/users");
-const posts = require("./data/posts");
 
-// // index route for Users
-// app.get("/api/users", (req, res) => {
-//     res.json(users);
-// });
+// Logging Middlewaare
+app.use((req, res, next) => {
+    const time = new Date();
 
-// // show route for users
-// app.get("/api/users/:id", (req, res) => {
-//     const user = users.find((u) => u.id == req.params.id);
-//     if (user) {
-//         res.json(user);
-//     } else {
-//         res.send("User Not Found")
-//     }
-// });
+    console.log(
+        `-----
+${time.toLocaleTimeString()}: Received a ${req.method} request to ${req.url}.`
+    );
+    if (Object.keys(req.body).length > 0) {
+        console.log("Containing the data:");
+        console.log(`${JSON.stringify(req.body)}`);
+    }
+    next();
+});
 
-// // index route for Posts
-// app.get("/api/posts", (req, res) => {
-//     res.json(posts);
-// });
+// Valid API Keys.
+apiKeys = ["perscholas", "ps-example", "hJAsknw-L198sAJD-l3kasx"];
 
-// // show route for posts
-// app.get("/api/posts/:id", (req, res) => {
-//     const post = posts.find((p) => p.id == req.params.id);
-//     if (post) {
-//         res.json(post);
-//     } else {
-//         res.send("Post not found")
-//     }
-// });
+// New middleware to check for API keys!
+// Note that if the key is not verified,
+// we do not call next(); this is the end.
+// This is why we attached the /api/ prefix
+// to our routing at the beginning!
+app.use("/api", function (req, res, next) {
+    var key = req.query["api-key"];
 
-// app.get("/", (req, res) => {
-//     res.send("All useable routes start with slash api");
-// });
+    // Check for the absence of a key.
+    if (!key) next(error(400, "API Key Required"));
 
+    // Check for key validity.
+    if (apiKeys.indexOf(key) === -1) next(error(401, "Invalid API Key"));
 
-// all routes
-app
-    .route("/api/users")
-    .get((req, res) => {
-        res.json(users);
-    })
-    .post((req, res) => {
-        if (req.body.name && req.body.username && req.body.email) {
-            if (users.find((u) => u.username == req.body.username)) {
-                res.json({ error: "Username Already Taken" });
-                return;
-            }
+    // Valid key! Store it in req.key for route access.
+    req.key = key;
+    next();
+});
 
-            // port listening info
-            app.listen(port, () => {
-                console.log(`Server listening on port: ${port}.`);
-            });
+// Use our Routes
+app.use("/api/users", users);
+app.use("/api/posts", posts);
+
+// Adding some HATEOAS links.
+app.get("/", (req, res) => {
+    res.json({
+        links: [
+            {
+                href: "/api",
+                rel: "api",
+                type: "GET",
+            },
+        ],
+    });
+});
+
+// Adding some HATEOAS links.
+app.get("/api", (req, res) => {
+    res.json({
+        links: [
+            {
+                href: "api/users",
+                rel: "users",
+                type: "GET",
+            },
+            {
+                href: "api/users",
+                rel: "users",
+                type: "POST",
+            },
+            {
+                href: "api/posts",
+                rel: "posts",
+                type: "GET",
+            },
+            {
+                href: "api/posts",
+                rel: "posts",
+                type: "POST",
+            },
+        ],
+    });
+});
+
+// 404 Middleware
+app.use((req, res, next) => {
+    next(error(404, "Resource Not Found"));
+});
+
+// Error-handling middleware.
+// Any call to next() that includes an
+// Error() will skip regular middleware and
+// only be processed by error-handling middleware.
+// This changes our error handling throughout the application,
+// but allows us to change the processing of ALL errors
+// at once in a single location, which is important for
+// scalability and maintainability.
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({ error: err.message });
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on port: ${port}.`);
+});
